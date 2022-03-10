7c6d7ad5fdb321ea53f98d129d3a0428cf1b898d
Adrian <adrianfdez469@gmail.com>
2022-03-10 18:34:18 -0500

    Modifying variable definitions on App cmp to object types

--- part1/src/Content.js
+++ part1/src/Content.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Part from './Part';
 
-const Content = ({part1,part2,part3,exercises1, exercises2, exercises3}) => {
+const Content = ({part1,part2,part3}) => {
 
   return (
     <>
-      <Part name={part1} number={exercises1}/>
-      <Part name={part2} number={exercises2}/>
-      <Part name={part3} number={exercises3}/>
+      <Part name={part1.name} number={part1.exercises}/>
+      <Part name={part2.name} number={part2.exercises}/>
+      <Part name={part3.name} number={part3.exercises}/>
     </>
   )
 
