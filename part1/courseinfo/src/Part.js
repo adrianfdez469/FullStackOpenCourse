2da4ac19f6e1fd51b669423851d396e7981010a6
Adrian <adrianfdez469@gmail.com>
2022-03-10 21:31:11 -0500

    Changing name of courseinfo project

---
+++ part1/courseinfo/src/Part.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Part = ({name, number}) => {
+
+  return (
+    <p>
+        {name} {number}
+    </p>
+  )
+
+}
+export default Part;
---
+++ part1/courseinfo/src/Total.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Total = (props) => {
+  const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises;
+  return <p>Number of exercises {total}</p>
+}
+export default Total;
