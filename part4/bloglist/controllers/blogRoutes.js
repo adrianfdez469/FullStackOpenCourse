20ebc6df357ba4cca201a104be406b2dc77bc56f
Adrian <adrianfdez469@gmail.com>
2022-03-15 23:22:16 -0500

    Exercise 4.2

---
+++ part4/bloglist/controllers/blogRoutes.js
@@ -0,0 +1,22 @@
+const route = require('express').Router()
+const Blog = require('../models/blogModel')
+
+route.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+route.post('/', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+module.exports = route
