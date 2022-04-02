29a98f7e76dbfca9f9b2059bab6b97224dbc5025
Adrian <adrianfdez469@gmail.com>
2022-04-02 11:07:40 -0500

    Removing unused code

--- part6/redux-anecdotes-main/src/components/AnecdoteList.js
+++ part6/redux-anecdotes-main/src/components/AnecdoteList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { vote } from '../reducers/anecdoteReducer'
-import { setNotification, showNotification } from '../reducers/notificationReducer'
+import { showNotification } from '../reducers/notificationReducer'
 
 const AnecdoteList = props => {
   
